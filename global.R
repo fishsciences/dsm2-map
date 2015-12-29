dbdd23f524fdcf0a0ab15e933c2685dff900eb5a
Travis Hinkelman <travis.hinkelman@gmail.com>
2015-12-29 09:57:53 -0800

    Update channel shapefiles. Add node shapefiles. Add control element.

--- global.R
+++ global.R
@@ -1,8 +1,9 @@
 library(rgdal)
 library(leaflet)
 
-flowlines = readOGR(dsn = ".", layer="FlowlinesLatLong")
-ibdpm = readOGR(dsn = ".", layer = "IBDPMChannelsLatLong")
+flowlines = readOGR(dsn = "./shapefiles", layer="FlowlinesLatLong")
+ibdpm = readOGR(dsn = "./shapefiles", layer = "IBDPMChannelsLatLong")
+nodes = readOGR(dsn = "./shapefiles", layer = "NodesLatLong")
 
 map = leaflet() %>% 
   addTiles(group = "Map") %>%
@@ -12,8 +13,9 @@ map = leaflet() %>%
                                                                                                          "Length (km):", round(km, 1))) %>%
   addPolylines(data = ibdpm, color = "darkred", weight = 4, group = "IB-DPM Channels", popup = ~paste("Channel", channel_nu, "<br>",
                                                                                                       "Length (km):", round(km, 1))) %>%
+  addCircles(data = nodes, color = "black", radius = 20, group = "DSM2 Nodes", popup = ~paste("Node", NNUM)) %>%
   addLayersControl(
-    overlayGroups = c("All DSM2 Channels", "IB-DPM Channels"),
+    overlayGroups = c("All DSM2 Channels", "IB-DPM Channels", "DSM2 Nodes"),
     baseGroups = c("Map", "Aerial"),
     options = layersControlOptions(collapsed = FALSE)
   )
