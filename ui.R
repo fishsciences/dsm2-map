727bc00cf6ae04d5d3526d3d66e88650b3df9a17
Travis Hinkelman <travis.hinkelman@gmail.com>
2017-09-21 22:06:59 -0700

    Sort channels
    Sort channels in selectInput

--- ui.R
+++ ui.R
@@ -6,6 +6,6 @@ shinyUI(bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput('Map', width = "100%", height = "100%"),
   absolutePanel(top = 10, left = 50, 
-                selectInput("selected_channel", "Selected channel", c("", cll$channel_nu), selected = ""))
+                selectInput("selected_channel", "Selected channel", c("", sort(cll$channel_nu)), selected = ""))
 ))
 
